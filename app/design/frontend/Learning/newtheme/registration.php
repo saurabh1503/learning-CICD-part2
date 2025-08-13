6333d0332ed3b8d04f7bf7d17c7b8f8d6f5b3681
root <root@Saurabh-PC.>
2025-08-13 01:19:24 +0000

    Test CI/CD workflow

---
+++ app/design/frontend/Learning/newtheme/registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ * magento learning
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Learning/newtheme', __DIR__);
